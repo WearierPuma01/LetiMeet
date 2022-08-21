fb23e0d92ba76c2b1fead687a2e298d3ffad1d3f
WearierPuma01 <mr.bochka003@gmail.com>
2022-08-21 20:58:40 +1000

    Project creation

---
+++ src/main/java/com/etu/letimeet/LetiMeetApplication.java
@@ -0,0 +1,13 @@
+package com.etu.letimeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetiMeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetiMeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/etu/letimeet/LetiMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etu.letimeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetiMeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
